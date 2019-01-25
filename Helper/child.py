2e320bc1a9ff747f5b0a9eaf58973c3b5cbe413c
Bruno <bgarcia@lcc.uma.es>
2019-01-25 12:43:30 +0100

    Move temporal folder handling to base

--- Helper/child.py
+++ Helper/child.py
@@ -1,8 +1,13 @@
 from Helper import Log, Level
+from os.path import realpath, exists
+from os import makedirs
 import threading
+from tempfile import TemporaryDirectory
 
 
 class Child:
+    TEMP_BASE = 'Temp'
+
     def __init__(self, name: str):
         self.name = name
         self.thread = threading.Thread(
@@ -10,6 +15,7 @@ class Child:
             daemon=True
         )
         self.stopRequested = False
+        self.TempFolder = None
 
     def Broadcast(self, level: Level, msg: str):
         Log.Log(level, f'[{self.name}{self.thread.ident}] {msg}')
@@ -24,9 +30,13 @@ class Child:
         self.stopRequested = True
 
     def _runWrapper(self):
-        Log.OpenLogFile(self.name)
-        self.Run()
-        Log.CloseLogFile(self.name)
+        basefolder = realpath(self.TEMP_BASE)
+        if not exists(basefolder): makedirs(basefolder)
+        with TemporaryDirectory(dir=basefolder) as tempFolder:
+            self.TempFolder = tempFolder
+            Log.OpenLogFile(self.name)
+            self.Run()
+            Log.CloseLogFile(self.name)
 
     def Run(self):
         raise NotImplementedError()
