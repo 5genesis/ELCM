b0ce9c9a5701c530a676f8c6a42392961d334563
Bruno <bgarcia@lcc.uma.es>
2019-02-12 15:21:21 +0100

    Add history pagination

--- Scheduler/routes.py
+++ Scheduler/routes.py
@@ -1,11 +1,12 @@
 from Scheduler import app
 from Status import Status, ExperimentQueue
 from Experiment import Experiment
-from flask import render_template, make_response
+from flask import render_template, make_response, request
 from functools import wraps, update_wrapper
 from datetime import datetime
 from Helper import Log, Serialize
 from typing import List, Dict
+from flask_paginate import Pagination, get_page_parameter
 
 
 # https://arusahni.net/blog/2014/03/flask-nocache.html
@@ -39,4 +40,11 @@ def history():
     for id in reversed(sorted(ids)):
         digest = Experiment.Load(id)
         experiments.append(digest)
-    return render_template('history.html', experiments=experiments)
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    pagination = Pagination(page=page, total=len(experiments), search=False,
+                            record_name='experiments', per_page=10, bs_version=4,
+                            display_msg='Displaying <b>{start} - {end}</b> {record_name} (out of <b>{total}</b>)')
+    start = pagination.skip
+    end = start + pagination.per_page
+    experiments = experiments[start:end]
+    return render_template('history.html', experiments=experiments, pagination=pagination)
