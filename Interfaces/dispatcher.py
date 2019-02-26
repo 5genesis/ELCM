1097904c2c24faba2ca433f32ea12cd454b5d947
Bruno <bgarcia@lcc.uma.es>
2019-02-26 12:04:25 +0100

    Correctly set Content-Type

--- Interfaces/dispatcher.py
+++ Interfaces/dispatcher.py
@@ -5,11 +5,11 @@ from threading import Thread
 
 class DispatcherApi(RestClient):
     def __init__(self, host, port):
-        super().__init__(host, port, '/api/executor')
+        super().__init__(host, port, '/api')
 
     def UpdateStatus(self, executionId: int, status: str):
         Thread(target=self.updateAsync, args=(executionId, status)).start()
 
     def updateAsync(self, executionId: int, status: str):
         url = f'{self.api_url}/execution/{executionId}'
-        self.httpPatch(url, None, json.dumps({'Status': status}))
+        self.httpPatch(url, {'Content-Type': 'application/json'}, json.dumps({'Status': status}))
