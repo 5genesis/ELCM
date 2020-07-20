77b69c9be3cf360aef3f37001e9fdfa18dac6522
Bruno Garcia <bgarcia@lcc.uma.es>
2020-07-20 15:45:59 +0200

    Add scenario handling in NEST creation

--- Composer/composer.py
+++ Composer/composer.py
@@ -3,7 +3,7 @@ from Data import ExperimentDescriptor, ExperimentType, NsInfo, Metal
 from .platform_configuration import PlatformConfiguration, TaskDefinition
 from importlib import import_module
 from Helper import Log
-from typing import List, Dict
+from typing import List, Dict, Optional, Tuple
 from Executor.Tasks.Run import Message
 from Interfaces import Management
 
@@ -34,6 +34,7 @@ class Composer:
 
         actions: List[ActionInformation] = []
         panels: List[DashboardPanel] = []
+        errored = False
 
         if len(descriptor.NetworkServices) != 0:
             sliceManager = Management.SliceManager()
@@ -47,31 +48,39 @@ class Composer:
                     nsInfo.Requirements = requirements
                     configuration.NetworkServices.append(nsInfo)
                 except Exception as e:
+                    errored = True
                     actions.append(
                         _messageAction("ERROR",
                                        f"Exception while obtaining information about network service {nsId}: {e}"))
 
-        if len(configuration.NetworkServices) != 0:  # At least one NSD info was successfully retrieved
-            configuration.Nest = cls.composeNest(descriptor.Slice, descriptor.Scenario, configuration.NetworkServices)
-
-        if descriptor.Type == ExperimentType.MONROE:
-            actions.extend(cls.facility.GetMonroeActions())
-        else:
-            if descriptor.Automated:
-                for ue in descriptor.UEs:
-                    actions.extend(cls.facility.GetUEActions(ue))
-                for testcase in descriptor.TestCases:
-                    testcaseActions = cls.facility.GetTestCaseActions(testcase)
-                    if len(testcaseActions) != 0:
-                        actions.extend(testcaseActions)
-                    else:
-                        actions.append(_messageAction("ERROR", f'TestCase "{testcase}" did not generate any actions'))
-                    panels.extend(cls.facility.GetTestCaseDashboards(testcase))
+        if not errored:
+            nest, error = cls.composeNest(descriptor.Slice, descriptor.Scenario, configuration.NetworkServices)
+            if error is None:
+                configuration.Nest = nest
             else:
-                delay = ActionInformation()
-                delay.TaskName = "Run.Delay"
-                delay.Config = {'Time': descriptor.Duration*60}
-                actions.append(delay)
+                errored = True
+                actions.append(_messageAction("ERROR", f'Error while generating NEST data for experiment: {error}'))
+
+        if not errored:
+            if descriptor.Type == ExperimentType.MONROE:
+                actions.extend(cls.facility.GetMonroeActions())
+            else:
+                if descriptor.Automated:
+                    for ue in descriptor.UEs:
+                        actions.extend(cls.facility.GetUEActions(ue))
+                    for testcase in descriptor.TestCases:
+                        testcaseActions = cls.facility.GetTestCaseActions(testcase)
+                        if len(testcaseActions) != 0:
+                            actions.extend(testcaseActions)
+                        else:
+                            actions.append(_messageAction("WARNING",  # Notify, but do not cancel execution
+                                                          f'TestCase "{testcase}" did not generate any actions'))
+                        panels.extend(cls.facility.GetTestCaseDashboards(testcase))
+                else:
+                    delay = ActionInformation()
+                    delay.TaskName = "Run.Delay"
+                    delay.Config = {'Time': descriptor.Duration*60}
+                    actions.append(delay)
 
         actions.sort(key=lambda action: action.Order)  # Sort by Order
         requirements = set()
@@ -104,24 +113,27 @@ class Composer:
             return None
 
     @classmethod
-    def composeNest(cls, baseSlice: str, scenario: str, nss: List[NsInfo]) -> Dict:
-        # TODO: Handle scenario
-        nsList = []
-        for ns in nss:
-            nsList.append({
-                "nsd-id": ns.Id,
-                "placement": ns.Location,
-            })
+    def composeNest(cls, baseSlice: str, scenario: str, nss: List[NsInfo]) -> Tuple[Dict, Optional[str]]:
+        """Returns the NEST as Dict (possibly empty) and a string indicating the error (None in case of success)"""
+        try:
+            nsList = []
+            for ns in nss:
+                nsList.append({
+                    "nsd-id": ns.Id,
+                    "placement": ns.Location,
+                })
 
-        return {
-            "base_slice_descriptor": {
-                "base_slice_des_id": baseSlice
-            },
-            "service_descriptor": {
-                "ns_list": nsList
-            }
-        }
+            scenarioData = Facility.Scenarios().get(scenario, None)
+            if scenarioData is None:
+                raise RuntimeError("Unrecognized scenario '{scenario}'")
 
+            sliceDescriptor = {"base_slice_des_id": baseSlice}
+            sliceDescriptor.update(scenarioData)
 
+            nest = {"base_slice_descriptor": sliceDescriptor}
+            if len(nsList) != 0:
+                nest["service_descriptor"] = {"ns_list": nsList}
 
-
+            return nest, None
+        except Exception as e:
+            return {}, str(e)
