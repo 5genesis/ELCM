0e4ad2d7bb17239aaceaa9f9b6cfb8da8d8cc5c8
Bruno Garcia <bgarcia@lcc.uma.es>
2020-12-16 10:13:55 +0100

    Add PostRun coordination step

--- Executor/Tasks/PostRun/__init__.py
+++ Executor/Tasks/PostRun/__init__.py
@@ -1,2 +1,3 @@
 from .decommission import Decommission
 from .release_resources import ReleaseResources
+from .farewell import Farewell
---
+++ Executor/Tasks/PostRun/farewell.py
@@ -0,0 +1,41 @@
+from Task import Task
+from Helper import Level, Config
+from time import sleep
+from Interfaces import RemoteApi
+
+
+class Farewell(Task):
+    def __init__(self, logMethod, parent):
+        super().__init__("Farewell", parent, logMethod, None)
+
+    def Run(self):
+        remote = self.parent.Descriptor.Remote
+        if remote is not None:
+            eastWest = Config().EastWest
+            if eastWest.Enabled:
+                from Experiment import ExperimentStatus
+
+                remoteApi: RemoteApi = self.parent.RemoteApi
+                remoteId = self.parent.RemoteId
+
+                self.Log(Level.INFO, "Waiting for remote side to reach Post-Run (or higher)")
+                status = ExperimentStatus.Init
+                retries = 5
+
+                while status.value < ExperimentStatus.PostRun.value and retries > 0:
+                    status, _ = remoteApi.GetStatus(remoteId)
+                    if status is None:
+                        self.Log(Level.WARNING, f"Could not retrieve status from remote side (Retries: {retries})")
+                        status = ExperimentStatus.Init
+                        retries -= 1
+                        sleep(5)
+
+                if status.value >= ExperimentStatus.PostRun.value:
+                    self.Log(Level.INFO, f"Remote side finished Run stage with status {status.name}")
+                else:
+                    self.Log(Level.ERROR, "Could not coordinate execution end with remote. " 
+                                          "Remote side results may be inconsistent.")
+            else:
+                raise RuntimeError("Unable to run distributed experiment while East/West interface is disabled.")
+        else:
+            self.Log(Level.INFO, 'Remote not set, skipping farewell.')
